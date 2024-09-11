d795d5719f80c59cd6baa5c71b5389bd5fef0f5e
Sergio Manrique Ambriz <l24120376@.morelia.tecnm.mx>
2024-09-10 18:34:02 -0600

    actualice

--- tarea5U2/src/Cuadrado.java
+++ tarea5U2/src/Cuadrado.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Cuadrado {
-}
+
+      public  Double ladoUno;
+      public  Double alturaUno;
+
+
+
+        public Cuadrado(Double ladoUno, Double alturaUno) {
+
+            this.ladoUno = ladoUno;
+            this.alturaUno = alturaUno;
+        }
+
+        public String perimetrorectangulo(){
+            return "El perimetro del rectangulo es: "+(this.alturaUno*2+
+                    this.ladoUno*2);
+
+        }
+        public String arearectangulo(){
+
+            return "El area del rectangulo es: "+(this.ladoUno*this.alturaUno);
+        }
+
+
+            public String perimetrorectangulo(Integer ladoUno, Integer alturaUno) {
+                return "El perimetro del rectangulo es: " + (this.alturaUno * 2 +
+                        this.ladoUno * 2);
+            }
+
+                public String arearectangulo(double ladoUno, double alturaUno){
+                    return "El aread del rectangulo es: " + (this.ladoUno * this.alturaUno);
+                }
+            }
+
+
+
+
+
+
